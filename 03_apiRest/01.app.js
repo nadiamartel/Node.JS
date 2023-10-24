81fc0a4af7b3ac26846007e1f57e5a5073eb808a
nadiamartel <nadiagmartel@gmail.com>
2023-10-24 18:41:08 -0300

    method SOME

--- 03_apiRest/01.app.js
+++ 03_apiRest/01.app.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const movies = require("./movies.json")
 const app = express()
 
 app.disable("x-powered-by") //quita el header x-powered-by: Express
@@ -6,6 +7,25 @@ app.disable("x-powered-by") //quita el header x-powered-by: Express
 app.get("/", (req, res) =>{
     res.json({ message: "Hola Mundo" })
 })
+
+//Todos los recurso que sean Movies se identifican con /movies
+app.get("/movies", (req, res) =>{
+    const { genre } = req.query
+    if(genre){
+        const moviesFiltered = movies.filter(movie => movie.genre.some(g => g.toLocaleLowerCase() === genre.toLocaleLowerCase()))
+        return res.json(moviesFiltered)
+    }
+    res.json(movies)
+})
+
+app.get("/movies/:id", (req, res) =>{
+    const { id } = req.params
+    const movie = movies.find(movie => movie.id === id)
+    if(movie) return res.json(movie)
+
+    res.status(404).json({ message: "Movie not found" })
+})
+
 
 const PORT = process.env.PORT ?? 1234
 
