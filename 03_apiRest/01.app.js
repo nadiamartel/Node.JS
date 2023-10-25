c78514c583d1a615c89429f6dfe4afc6abf39ab3
nadiamartel <nadiagmartel@gmail.com>
2023-10-25 17:12:54 -0300

    add ZOD

--- 03_apiRest/01.app.js
+++ 03_apiRest/01.app.js
@@ -1,8 +1,11 @@
 const express = require("express")
 const movies = require("./movies.json")
+const crypto = require("node:crypto")
+const { validateMovie } = require("./schemas/movies")
 const app = express()
 
 app.disable("x-powered-by") //quita el header x-powered-by: Express
+app.use(express.json())
 
 app.get("/", (req, res) =>{
     res.json({ message: "Hola Mundo" })
@@ -26,6 +29,31 @@ app.get("/movies/:id", (req, res) =>{
     res.status(404).json({ message: "Movie not found" })
 })
 
+app.post("/movies", (req, res) =>{
+    const result = validateMovie(req.body)
+
+    if(result.error){
+        return res.status(400).json({ error: JSON.parse(result.error.message) })
+    }
+
+    const newMovie = {
+        id: crypto.randomUUID(),
+        ...result.data
+        // title,
+        // genre,
+        // year,
+        // director,
+        // duration,
+        // rate: rate ?? 0,
+        // poster
+    }
+    
+    //esto no seria REST porq queda guardado en el estado de aplicacion en memoria:
+    movies.push(newMovie)
+    
+    res.status(201).json(newMovie)
+})
+    
 
 const PORT = process.env.PORT ?? 1234
 
---
+++ 03_apiRest/schemas/movies.js
@@ -0,0 +1,35 @@
+const z = require("zod")
+
+const movieSchema = z.object({
+    tittle: z.string({
+        invalid_type_error: "Movie tittle must be a string",
+        required_error: "Movie tittle is required"
+    }),
+    year: z.number().int().min(1990).max(2024),
+    director: z.string(),
+    duration: z.number().int().positive(),
+    rate: z.number().min(0).max(10).default(5),
+    poster: z.string().url({
+        message: 'Poster must be a valid URL'
+    }),
+    genre: z.array(
+        z.enum(['Action', 'Adventure', 'Crime', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Thriller', 'Sci-Fi']),
+        {
+            invalid_type_error:"Movie genre must be an array of enum Genre",
+            required_error: "Movie genre is required."
+        }
+    )
+})
+
+function validateMovie(input){
+    return movieSchema.safeParse(input)
+}
+
+function validatePartialMovie(input){
+    return movieSchema.partial().safeParse(input)
+}
+
+module.exports = {
+    validateMovie,
+    validatePartialMovie
+}
