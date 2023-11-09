8da890d0900213503b507d78c289424020796606
nadiamartel <nadiagmartel@gmail.com>
2023-11-09 11:38:43 -0300

    add controllers

---
+++ 04_back/controllers/movies.js
@@ -0,0 +1,18 @@
+import { MovieModel } from '../models/local-file-system/movie.js'
+// import { MovieModel } from '../models/database/movie.js'
+import { validateMovie, validatePartialMovie } from '../schemas/movies.js'
+
+export class MovieController {
+  static async getAll (req, res) {
+    const { genre } = req.query
+    const movies = await MovieModel.getAll({ genre })
+    res.json(movies)
+  }
+
+  static async getById (req, res) {
+    const { id } = req.params
+    const movie = await MovieModel.getById({ id })
+    if (movie) return res.json(movie)
+    res.status(404).json({ message: 'Movie not found' })
+  } 
+}
