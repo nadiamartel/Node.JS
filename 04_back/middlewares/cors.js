05bfcf15ddceea054c91f19f6d329fe06ddba0cb
nadiamartel <nadiagmartel@gmail.com>
2023-11-12 22:02:34 -0300

    fix

--- 04_back/middlewares/cors.js
+++ 04_back/middlewares/cors.js
@@ -1,22 +1,22 @@
 import cors from 'cors'
 
 const ACCEPTED_ORIGINS = [
-  'http://localhost:8080',
-  'http://localhost:1234',
-  'https://movies.com',
-  'https://midu.dev'
+    'http://127.0.0.1:5500',
+    'http://localhost:8080',
+    'http://localhost:1234',
+    'https://movies.com'
 ]
 
 export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) => cors({
-  origin: (origin, callback) => {
-    if (acceptedOrigins.includes(origin)) {
-      return callback(null, true)
-    }
+    origin: (origin, callback) => {
+        if (acceptedOrigins.includes(origin)) {
+            return callback(null, true)
+        }
 
-    if (!origin) {
-      return callback(null, true)
-    }
+        if (!origin) {
+            return callback(null, true)
+        }
 
-    return callback(new Error('Not allowed by CORS'))
-  }
+        return callback(new Error('Not allowed by CORS'))
+    }
 })
